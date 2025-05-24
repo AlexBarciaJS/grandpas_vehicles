37855a2e807c6e8b3f7c6c907762498ca381e3ae
AlexBarciaJS <guillermoalexisbarcia@gmail.com>
2025-05-24 17:31:57 -0500

    fix: card side color

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,73 @@
+import { useState } from "react";
 import type { CardProps } from "../../types";
+import "./Card.css";
 
-const myStyle = {
-  width: "18rem",
-};
+const emptyColor = "white";
+const backgroundColor1 = "cadetblue";
+const backgroundColor2 = "antiquewhite";
 
 export const Card: React.FC<CardProps> = ({ image, description }) => {
+  const [cardSide, setCardSide] = useState(-1);
+  const [background, setBackground] = useState(emptyColor);
+
+  const imgStyle = {
+    transform: `scaleX(${cardSide})`,
+    backgroundColor: background,
+  };
+
+  const buttonLeft = {
+    background: backgroundColor1,
+  };
+
+  const buttonRight = {
+    background: backgroundColor2,
+  };
+
+  const changeBackground = (color: string, side: number): string => {
+    setBackground(color);
+    setCardSide(side);
+    return background;
+  };
+
   return (
-    <div className="card" style={myStyle}>
-      <img src={image} className="card-img-top" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">{description}</p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+    <>
+      <div
+        className="btn-group"
+        role="group"
+        aria-label="Basic mixed styles example"
+      >
+        <button
+          onClick={() => changeBackground(backgroundColor1, -1)}
+          type="button"
+          className="btn"
+          style={buttonLeft}
+        >
+          Left
+        </button>
+        <button
+          onClick={() => changeBackground(backgroundColor2, 1)}
+          type="button"
+          className="btn"
+          style={buttonRight}
+        >
+          Right
+        </button>
       </div>
-    </div>
+      <div className="card card-content">
+        <img
+          src={image}
+          className="card-img-top"
+          style={imgStyle}
+          alt="..."
+        ></img>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">{description}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
